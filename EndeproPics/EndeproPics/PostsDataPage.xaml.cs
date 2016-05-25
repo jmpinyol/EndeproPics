148c71130d7365016e1e1e5891143381ae1d1995
Josep Maria Pinyol Fontseca <jmpinyol@endepro.com>
2016-05-25 13:18:29 +0200

    First commit of project

---
+++ EndeproPics/EndeproPics/PostsDataPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace EndeproPics
+{
+    public partial class PostsDataPage : Xamarin.Forms.Pages.ListDataPage
+    {
+        public PostsDataPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
